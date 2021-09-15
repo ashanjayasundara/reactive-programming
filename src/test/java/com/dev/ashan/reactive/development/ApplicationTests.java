d35e10dcbdbef47b13e79a305ebf38053a1dc0ca
Ashan Jayasundara <email2ashan@gmail.com>
2021-09-15 13:40:30 +0530

    first commit

---
+++ src/test/java/com/dev/ashan/reactive/development/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ashan.reactive.development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
